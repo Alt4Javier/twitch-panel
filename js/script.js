5608f26621055c9980952b3e17f8de4cc8b53578
Alt 4 Javier <71192210+Alt4Javier@users.noreply.github.com>
2021-07-30 11:43:21 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () =>{
+    const loader_animation = document.querySelector('.loader_animation')
+    loader_animation.style.opacity = 0
+    loader_animation.style.visibility = 'hidden'
+})
